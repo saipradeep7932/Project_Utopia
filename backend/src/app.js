d668a85ae92aa171cc0aef2156b133c5f61284bb
saipradeep7932 <saipradeep7931@gmail.com>
2025-12-12 01:53:27 +0530

    Final Commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,10 @@ const app= express();
 app.use(express.json());
 
 import userRoutes from './routes/user.route.js';
-// import postRoutes from './routes/post.route.js'; 
+import postRoutes from './routes/post.route.js'; 
 
 //routes declaration
 app.use("/api/v1/users",userRoutes);
-// app.use("/api/v1/posts",postRoutes);
+app.use("/api/v1/posts",postRoutes);
 
 export default app;
